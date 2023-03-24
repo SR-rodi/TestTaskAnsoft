23f6f58915b9a292832674be71e77ff55ff900e0
Sergey Rodin <rodin_sr@mail.ru>
2023-03-25 00:06:33 +0300

    Добавил фрагмент с гугл картами

--- app/src/main/java/com/example/testtaskansoft/feature/presentation/delivery/DeliveriesViewModel.kt
+++ app/src/main/java/com/example/testtaskansoft/feature/presentation/delivery/DeliveriesViewModel.kt
@@ -3,8 +3,6 @@ package com.example.testtaskansoft.feature.presentation.delivery
 import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.testtaskansoft.core.provider.DispatcherIoProvider
-import com.example.testtaskansoft.feature.data.api.DeliveryApi
 import com.example.testtaskansoft.feature.domain.usecase.AllDeliveryUseCase
 import com.example.testtaskansoft.feature.domain.usecase.CompleteDeliveryUseCase
 import kotlinx.coroutines.Dispatchers
@@ -13,14 +11,13 @@ import kotlinx.coroutines.launch
 class DeliveriesViewModel(
     private val allDeliveryUseCase: AllDeliveryUseCase,
     private val completeDeliveryUseCase: CompleteDeliveryUseCase,
-    private val api:DeliveryApi
 ) : ViewModel() {
 
 
     fun getDelivery() {
         viewModelScope.launch(Dispatchers.IO) {
             try {
-                Log.e("KArt", api.getAllDelivery().toString())
+                Log.e("KArt", allDeliveryUseCase.getAllDelivery().toString())
             } catch (e:Throwable){
                 Log.e("KArt", e.toString())
             }
--- app/src/main/java/com/example/testtaskansoft/feature/presentation/maps/MapsFragment.kt
+++ app/src/main/java/com/example/testtaskansoft/feature/presentation/maps/MapsFragment.kt
@@ -3,8 +3,14 @@ package com.example.testtaskansoft.feature.presentation.maps
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
+import com.example.testtaskansoft.R
 import com.example.testtaskansoft.core.base.BaseFragment
 import com.example.testtaskansoft.databinding.FragmentMapsBinding
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MapsFragment : BaseFragment<FragmentMapsBinding>() {
@@ -13,11 +19,18 @@ class MapsFragment : BaseFragment<FragmentMapsBinding>() {
 
     private val viewModel by viewModel<MapsViewModel>()
 
+    private val callback = OnMapReadyCallback { googleMap ->
+        val baseLocation = LatLng(55.7522, 37.6156)
+        googleMap.addMarker(MarkerOptions().position(baseLocation).title("Base Location"))
+        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(baseLocation, 10f))
+    }
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
+        mapFragment?.getMapAsync(callback)
         viewModel
-        // viewModel.getDelivery()
     }
 }
